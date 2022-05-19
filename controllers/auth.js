bbcc0944884a5ea292e351eefcfb69f35de4e70f
rahul7266 <singhrahul4620011@gmail.com>
2022-05-19 12:24:57 +0530

    async

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,8 @@ const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
 
+const { validationResult } = require('express-validator/check') ;
+
 const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
 
@@ -9,7 +11,7 @@ const { google } = require('googleapis');
 const CLIENT_ID = '468994077411-u7jmn0804bqk6b952ugqenq0k0m36pul.apps.googleusercontent.com';
 const CLEINT_SECRET = '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';
 const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-const REFRESH_TOKEN = '1//04A40MB87ZcJ8CgYIARAAGAQSNwF-L9IrkE27VBrH_jMFO7tpXmKdu1TaG__I7d7OezMJc0DXeCXe9cQq-CYhJCA_Yx8b_V5Q_q0';
+const REFRESH_TOKEN = '1//04iVa9HwaTUy6CgYIARAAGAQSNwF-L9Ir2XgAEFvkJ0L8bhXD97pB1ZYvheXIxgI7HUeR71e2biYwbEmOmtbmIsz1QrjHWSiTvMk';
 
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
@@ -37,9 +39,9 @@ const emailSend = (p) => {
         },
       });
 
-      const mailOptions = p;
+      // const mailOptions = p;
 
-      const result = await transport.sendMail(mailOptions);
+      const result = await transport.sendMail(p);
       return result;
     } catch (error) {
       return error;
@@ -54,7 +56,6 @@ const emailSend = (p) => {
 }
 
 
-
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
   if (message.length > 0) {
@@ -65,7 +66,12 @@ exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    errorMessage: message
+    errorMessage: message,
+    oldInput: {
+      email: '',
+      password: ''
+    },
+    validationErrors: []
   });
 };
 
@@ -83,7 +89,13 @@ exports.getSignup = (req, res, next) => {
     path: '/signup',
     pageTitle: 'Signup',
     isAuthenticated: false,
-    errorMessage: message
+    errorMessage: message,
+    oldInput:{
+      email:'',
+      password:'' ,
+      confirmPassword:''
+    },
+    validationErrors:[]
   });
 
 };
@@ -91,11 +103,34 @@ exports.getSignup = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render('auth/login', {
+      path: '/login',
+      pageTitle: 'Login',
+      errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password
+      },
+      validationErrors: errors.array()
+    });
+  }
+
   User.findOne({ email: email })
     .then(user => {
       if (!user) {
-        req.flash('error', 'Invalid email or password.');
-        return res.redirect('/login');
+        return res.status(422).render('auth/login', {
+          path: '/login',
+          pageTitle: 'Login',
+          errorMessage: 'Invalid email or password.',
+          oldInput: {
+            email: email,
+            password: password
+          },
+          validationErrors: []
+        });
       }
       bcrypt
         .compare(password, user.password)
@@ -108,28 +143,50 @@ exports.postLogin = (req, res, next) => {
               res.redirect('/');
             });
           }
-          req.flash('error', 'Invalid email or password.');
-          res.redirect('/login');
+          return res.status(422).render('auth/login', {
+            path: '/login',
+            pageTitle: 'Login',
+            errorMessage: 'Invalid email or password.',
+            oldInput: {
+              email: email,
+              password: password
+            },
+            validationErrors: []
+          });
         })
         .catch(err => {
           console.log(err);
           res.redirect('/login');
         });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error = new Error(err) ;
+      error.httpStarusCode = 500 ;
+      return next(error) ;
+    });
 };
 
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
-  User.findOne({ email: email })
-    .then(userDoc => {
-      if (userDoc) {
-        req.flash('error', 'E-Mail exists already, please pick a different one.');
-        return res.redirect('/signup');
-      }
-      return bcrypt
+  const errors = validationResult(req) ;
+  if(!errors.isEmpty())
+  {
+    return res.status(422).render('auth/signup', {
+
+      path: '/signup',
+      pageTitle: 'Signup',
+      isAuthenticated: false,
+      errorMessage: errors.array()[0].msg,
+      oldInput:{
+        email:email,
+        password:password ,
+        confirmPassword:req.body.confirmPassword
+      },
+      validationErrors:errors.array()
+    }) ;
+  }
+    bcrypt
         .hash(password, 12)
         .then(hashedPassword => {
           const user = new User({
@@ -149,11 +206,13 @@ exports.postSignup = (req, res, next) => {
             text: 'You Successfully Signedup !',
             html: '<h1>You Successfully Signedup !</h1>',
           });
+        })
+        .catch(err => {
+          const error = new Error(err) ;
+          error.httpStarusCode = 500 ;
+          return next(error) ;
         });
-    })
-    .catch(err => {
-      console.log(err);
-    });
+   
 };
 
 exports.postLogout = (req, res, next) => {
@@ -199,6 +258,7 @@ exports.postReset = (req, res, next) => {
         return user.save();
       })
       .then(result => {
+        // console.log('rahul') ;
         res.redirect('/');
         emailSend({
           to: req.body.email,
@@ -211,13 +271,16 @@ exports.postReset = (req, res, next) => {
         });
       })
       .catch(err => {
-        console.log(err);
+        const error = new Error(err) ;
+        error.httpStarusCode = 500 ;
+        return next(error) ;
       });
   });
 };
 
 
 exports.getNewPassword = (req, res, next) => {
+  
   const token = req.params.token;
   User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
     .then(user => {
@@ -227,15 +290,49 @@ exports.getNewPassword = (req, res, next) => {
       } else {
         message = null;
       }
+      console.log(user);
       res.render('auth/new-password', {
         path: '/new-password',
         pageTitle: 'New Password',
         errorMessage: message,
-        userId: user._id.toString()
+        userId: user._id.toString(),
+        passwordToken: token
       });
     })
     .catch(err => {
-      console.log(err);
+      const error = new Error(err) ;
+      error.httpStarusCode = 500 ;
+      return next(error) ;
     });
 };
 
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId
+  })
+    .then(user => {
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12)
+    })
+    .then(hashedPassword => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then(result => {
+      res.redirect('/login');
+    })
+    .catch(err => {
+      const error = new Error(err) ;
+      error.httpStarusCode = 500 ;
+      return next(error) ;
+    });
+};
